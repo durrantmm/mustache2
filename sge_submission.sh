9089f7f58b477ff1d1ddb67cc861e79609865fc9
Matt Durrant <durrantmm@gmail.com>
2017-11-03 14:34:51 -0600

    added

--- sge_submission.sh
+++ sge_submission.sh
@@ -28,4 +28,4 @@
 #
 #$ -cwd
 
-snakemake --cluster-config sge_luster.json --cluster "qsub -V -l h_vmem={cluster.mem} -l h_rt={cluster.time} -l s_rt={cluster.time} -A {cluster.account}" -j 200 -p
+snakemake --cluster-config sge_luster.json --cluster "qsub -V -l h_vmem={cluster.mem} -l h_rt={cluster.time} -l s_rt={cluster.time} -A {cluster.account} -o log_mustache -e log_mustache" -j 200 -p
