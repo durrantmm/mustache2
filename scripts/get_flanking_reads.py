d9aaa44a2bceed51e37869fb04f6e024ae01c4e9
Matt Durrant <durrantmm@gmail.com>
2017-11-06 13:33:48 -0700

    added

--- scripts/get_flanking_reads.py
+++ scripts/get_flanking_reads.py
@@ -17,6 +17,7 @@ def main():
     get_flanking_reads(genome_sam, insertseq_sam, fastq1, fastq2, class1, class2, genome_flanks_sam,
                        genome_noflanks_sam, insertseq_flanks_sam, insertseq_noflanks_sam)
 
+
 def read_class(path):
     with gzip.open(path) as infile:
         for line in infile:
@@ -26,20 +27,6 @@ def read_class(path):
             out.taxon = line[2]
             yield out
 
-def read_fastq(path):
-    with gzip.open(path) as infile:
-        while True:
-            try:
-                line1 = infile.readline().decode('UTF-8')
-                line2 = infile.readline().decode('UTF-8')
-                line3 = infile.readline().decode('UTF-8')
-                line4 = infile.readline().decode('UTF-8')
-                out = type("", (), {})()
-                out.name = line1.strip('@').strip().split()[0]
-                out.seq = line2.strip()
-                yield out
-            except:
-                break
 
 def read_sam_pairs(sam_path):
 
@@ -54,6 +41,7 @@ def read_sam_pairs(sam_path):
                 yield out
             except StopIteration:
                 break
+
 
 def reverse_complement(seq):
     out = ''
@@ -70,69 +58,41 @@ def reverse_complement(seq):
             out += c
     return out
 
-def add_taxon(r1, r2, fq1, fq2, c1, c2):
-    r1_seq = r1.query_sequence
 
-    if r1.is_reverse:
-        r1_seq = reverse_complement(r1_seq)
-
-    flip_read = False
-    if fq1.seq == r1_seq:
-        r1.set_tag('TX', c1.taxon)
-        r2.set_tag('TX', c2.taxon)
+def add_taxon(r1, r2, class1, class2):
+    if r1.is_read1:
+        r1.set_tag('TX', class1.taxon)
+        r2.set_tag('TX', class2.taxon)
+        out_gr = [r1, r2]
     else:
-        r1.set_tag('TX', c2.taxon)
-        r2.set_tag('TX', c1.taxon)
+        r1.set_tag('TX', class2.taxon)
+        r2.set_tag('TX', class1.taxon)
+        out_gr = [r2, r1]
 
-        flip_read = True
-
-    if not flip_read:
-        return (r1, r2)
-    else:
-        return (r2, r1)
+    return out_gr
 
 
-def adjust_read_order(gr1, gr2, ir1, ir2, fq1, fq2, class1, class2):
+def adjust_read_order(gr1, gr2, ir1, ir2, class1, class2):
 
-    gr1_seq = gr1.query_sequence
-    ir1_seq = ir1.query_sequence
-
-    if gr1.is_reverse:
-        gr1_seq = reverse_complement(gr1_seq)
-    if ir1.is_reverse:
-        ir1_seq = reverse_complement(ir1_seq)
-
-    flip_gr = False
-    if fq1.seq == gr1_seq:
+    if gr1.is_read1:
         gr1.set_tag('TX', class1.taxon)
         gr2.set_tag('TX', class2.taxon)
+        out_gr = [gr1, gr2]
     else:
         gr1.set_tag('TX', class2.taxon)
         gr2.set_tag('TX', class1.taxon)
+        out_gr = [gr2, gr1]
 
-        flip_gr = True
-
-    flip_ir = False
-    if fq1.seq == ir1_seq:
+    if ir1.is_read1:
         ir1.set_tag('TX', class1.taxon)
         ir2.set_tag('TX', class2.taxon)
+        out_ir = [ir1, ir2]
     else:
         ir1.set_tag('TX', class2.taxon)
         ir2.set_tag('TX', class1.taxon)
-        flip_ir = True
+        out_ir = [ir2, ir1]
 
-    out = [None, None, None, None]
-    if flip_gr and flip_ir:
-        out[0], out[1] = gr1, gr2
-    else:
-        out[0], out[1] = gr2, gr1
-
-    if not flip_ir:
-        out[2], out[3] = ir1, ir2
-    else:
-        out[2], out[3] = ir2, ir1
-
-    return out
+    return out_gr+out_ir
 
 def get_flanking_reads(genome_sam_path, insertseq_sam_path, fastq1_path, fastq2_path, class1_path, class2_path,
                        genome_flanks_sam_path, genome_noflanks_sam_path, insertseq_flanks_sam_path,
@@ -151,22 +111,20 @@ def get_flanking_reads(genome_sam_path, insertseq_sam_path, fastq1_path, fastq2_
 
     class1 = read_class(class1_path)
     class2 = read_class(class2_path)
-    fastq1 = read_fastq(fastq1_path)
-    fastq2 = read_fastq(fastq2_path)
 
     genome_reads = next(genome_sam)
     insertseq_reads = next(insertseq_sam)
     count = 0
 
     print("Beginning Read Processing")
-    for fq1, fq2, c1, c2 in zip(fastq1, fastq2, class1, class2):
+    for c1, c2 in zip(class1, class2):
         count += 1
 
         if c1.name == genome_reads.r1.query_name == insertseq_reads.r1.query_name:
 
             gr1, gr2, ir1, ir2 = adjust_read_order(genome_reads.r1, genome_reads.r2,
                                                    insertseq_reads.r1, insertseq_reads.r2,
-                                                   fq1, fq2, c1, c2)
+                                                   c1, c2)
             gr1.set_tag('IS', ir2.reference_name)
             gr2.set_tag('IS', ir1.reference_name)
             ir1.set_tag('GN', gr2.reference_name)
@@ -207,13 +165,13 @@ def get_flanking_reads(genome_sam_path, insertseq_sam_path, fastq1_path, fastq2_
             insertseq_reads = next(insertseq_sam)
 
         elif c1.name == genome_reads.r1.query_name:
-            gr1, gr2 = add_taxon(genome_reads.r1, genome_reads.r2, fq1, fq2, c1, c2)
+            gr1, gr2 = add_taxon(genome_reads.r1, genome_reads.r2, c1, c2)
             genome_noflanks_sam.write(gr1)
             genome_noflanks_sam.write(gr2)
             genome_reads = next(genome_sam)
 
         elif c1.name == insertseq_reads.r1.query_name:
-            ir1, ir2 = add_taxon(insertseq_reads.r1, insertseq_reads.r2, fq1, fq2, c1, c2)
+            ir1, ir2 = add_taxon(insertseq_reads.r1, insertseq_reads.r2, c1, c2)
             insertseq_noflanks_sam.write(ir1)
             insertseq_noflanks_sam.write(ir2)
             insertseq_reads = next(insertseq_sam)
